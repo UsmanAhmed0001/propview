e67f5be20c05427dc293eca89ff5e7bc1df4f884
Usman Ahmed <usmanahmed@UAs-MacBook-Pro.local>
2026-09-17 15:51:24 +0100

    fix: paste exact api key

--- JS/config.js
+++ JS/config.js
@@ -1,11 +1,14 @@
 export const CONFIG = {
   firebase: {
-    apiKey:            "AIzaSyDcd58FTGTur9LUW-GmhUCLmCcSOwFMJQk",
-    authDomain:        "spadepropview.firebaseapp.com",
-    projectId:         "spadepropview",
-    storageBucket:     "spadepropview.firebasestorage.app",
+    apiKey: "AIzaSyDcd58FTGTur9LUW-GmhUCLmCcSOwfMJQk",
+    authDomain: "spadepropview.firebaseapp.com",
+    projectId: "spadepropview",
+    storageBucket: "spadepropview.firebasestorage.app",
     messagingSenderId: "768409897058",
-    appId:             "1:768409897058:web:14b35979e121e7e2e5a93b",
+    appId: "1:768409897058:web:14b35979e121e7e2e5a93b",
+    measurementId: "G-RQ05HJM64W",
   },
   adminEmail: "ahmed_usman_farhan@outlook.com",
 };
+
+
